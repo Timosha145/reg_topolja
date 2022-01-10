8072ec794bb36c0910bdd3a05665d36308ac9fb6
Timosha145 <93324319+Timosha145@users.noreply.github.com>
2022-01-10 09:46:16 +0200

    Update reg_topolja.py

--- reg_topolja.py
+++ reg_topolja.py
@@ -1,16 +1,18 @@
 from random import *
 from module1 import *
-passwords=['Password789!']
-usernames=['Boris1']
+passwords=['A']
+usernames=['B']
 
-answer=''
+answerReg=''
 answerAboutPass=''
+passTry=1
+cor=''
 a=False
 
 
-while answer not in ['y','n','Y','N']:
-	answer=input('Добро пожаловать! Вы уже зарегистрированы?(y/n): ')
-if answer in ['N','n']:
+while answerReg not in ['y','n','Y','N']:
+	answerReg=input('Добро пожаловать! Вы уже зарегистрированы?(y/n): ')
+if answerReg in ['N','n']:
 	while a==False:
 		newUser=input('Придумайте себе имя пользователя не длинне 12 символов: ')
 		if len(newUser)>12:
@@ -24,15 +26,44 @@ if answer in ['N','n']:
 		answerAboutPass=input('Желаете авто генерацию пороля?(y/n): ')
 	while 1:
 		if answerAboutPass in ['y','Y']:
-			print('Ваш пароль -',autoPassGenerator())
+			newPass=autoPassGenerator()
+			print('Ваш пароль -',newPass)
 			answer=''
+		elif passTry==0 and answerAboutPass in ['n','N']:
+			break
 			while answer not in ['y','n','Y','N']:
 				answer=input(f'Перегенирировать пароль?(y/n): ')
 			if answer in ['n','N']:
 				break
-		while 1:
+		while passTry==1 and answerAboutPass in ['n','N']:
 			if answerAboutPass in ['n','N']:
-				newPass=input('Придумайте новый пароль для данного пользователя, которое будет содержать цифры, буквы нижнего и верхнего регистра, а также спец символы: ')
-			if isPassOkay(newPass)==True:
+				newPass=input('Придумайте новый пароль для данного пользователя, которое будет содержать цифры, буквы нижнего и верхнего регистра, а также спец символы и будет содержать минимум 8 символов: ')
+			if isPassOkay(newPass)==False:
 				print('К сожалению пароль не подходит')
-			else: print('Ваш пароль',)
+			else: 
+				while 1:
+					passAgain=input('Подтвердите ваш новый пароль: ')
+					if passAgain==newPass:
+						passTry=0
+						break
+					else:
+						print('К сожалению пароли не совпадают')
+						while answer not in ['y','n','Y','N']:
+							tryAgain=input('Желайте придумать новый пароль?(y/n): ')
+						if tryAgain in ['y','Y']:
+							break
+if answerReg in ['N','n']:
+	usernames.append(newUser)
+	passwords.append(newPass)
+print()
+print('Для подтверждения просим пройти этап авторизации')
+while 1:
+	logUsername=input('Введите ваше имя пользователя: ')
+	logPassword=input('Введите ваш пароль: ')
+	try:
+		cor=isUserCorrect(logUsername, logPassword, usernames, passwords)
+	except ValueError:
+		print('Упс что-то пошло не так')
+	if cor==True:
+		break
+print('Добро пожаловать в систему!')
